8f9c09126dbe73d93af324a3c7a83caf65139eb5
MissYou <125418233@qq.com>
2017-03-20 23:05:32 +0800

    ref: Ref fromt the code

--- app/models/log.go
+++ app/models/log.go
@@ -6,12 +6,12 @@ import (
 
 /** 日志  */
 type Log struct {
-	Id       int       // 日志ID
-	Time     time.Time `orm:"auto_now_add;type(datetime)"` // 记录时间
-	UserId   int       // 用户ID
-	IP       string    `orm:"size(20)"`            // ip
-	Address  string    `orm:"size(20)"`            // 地址
-	Drive    string    `orm:"size(20)"`            // 设备
-	UserName string    `orm:"size(20)"`            // 用户名
-	TimeOut  time.Time `orm:"null;type(datetime)"` // 离开时间
+	Id       int       																			// 日志ID
+	Time     time.Time `orm:"auto_now_add;type(datetime)"`  // 记录时间
+	UserId   int       																			// 用户ID
+	IP       string    `orm:"size(20)"`            					// ip
+	Address  string    `orm:"size(20)"`            					// 地址
+	Drive    string    `orm:"size(20)"`            					// 设备
+	UserName string    `orm:"size(20)"`            					// 用户名
+	TimeOut  time.Time `orm:"null;type(datetime)"` 					// 离开时间
 }
--- app/models/message.go
+++ app/models/message.go
@@ -6,16 +6,16 @@ import (
 
 /** 消息实体 */
 type Message struct {
-	Id         int       // MessageId 消息ID
-	PlanId     int       // userid接收对象ID
-	ToUserId   int       // userid发送对象ID
-	FromUserId int       // planeID飞机对象ID
-	IsRead     bool      `orm:"default(false)"`              // boole 是否已经被读取
-	Context    string    `orm:"type(text)"`                  // 消息内容
-	ImageUrl   string    `orm:"size(20)"`                    // 消息图片链接
-	SendTime   time.Time `orm:"auto_now_add;type(datetime)"` // 发送时间
-	ReadTime   time.Time `orm:"type(datetime)"`              // 接收时间
-	Address    string    `orm:"size(20)"`                    // 该飞机所在的地方
-	Log        float64   // 经度
-	Don        float64   // 维度
+	Id         int       																		 	// MessageId 消息ID
+	PlanId     int       																		 	// userid接收对象ID
+	ToUserId   int       																		 	// userid发送对象ID
+	FromUserId int       																		 	// planeID飞机对象ID
+	IsRead     bool      `orm:"default(false)"`              	// boole 是否已经被读取
+	Context    string    `orm:"type(text)"`                  	// 消息内容
+	ImageUrl   string    `orm:"size(20)"`                    	// 消息图片链接
+	SendTime   time.Time `orm:"auto_now_add;type(datetime)"` 	// 发送时间
+	ReadTime   time.Time `orm:"type(datetime)"`              	// 接收时间
+	Address    string    `orm:"size(20)"`                    	// 该飞机所在的地方
+	Log        float64   																			// 经度
+	Don        float64   																			// 维度
 }
