c22cf4129fbb6b04f1fc9ae5514338c5b5d2502b
Imissyou1314 <125418233@qq.com>
2017-05-17 11:08:03 +0800

    update: Update to use swagger

--- main.go
+++ main.go
@@ -8,8 +8,7 @@ import (
 
 func main() {
 	if beego.BConfig.RunMode == "dev" {
-		// beego.BConfig.WebConfig.DirectoryIndex = true
-
+		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
 
